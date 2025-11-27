f4c4f26c730279da2318329b455872e16a6f074a
NeoNexusU6 <NeoNexus>
2025-11-28 00:25:41 +0800

    Refactor ownership test examples for clarity and consistency

--- src/main.rs
+++ src/main.rs
@@ -7,27 +7,28 @@ fn main() {
     {
         let s1 = String::from("ownership test");
         let mut s2 = s1;
-
+        
+        let s3 = &mut s2; // 创建一个可变引用
         take_ownership(&mut s2);
         println!("{s2}, function out!");
     }
-    
+
     {
         let s1 = String::from("ownership test");
-        
+
         // 验证：打印 s1 指向的堆数据地址
         println!("s1 指向的堆地址: {:p}", s1.as_ptr());
         // 验证：打印 s1 变量本身的栈地址
         println!("s1 变量的栈地址: {:p}", &s1);
 
-        let mut s2 = s1; 
+        let mut s2 = s1;
         // 此时 s1 已经失效 (Move)，不能再打印 s1 了
 
         // 验证：打印 s2 指向的堆数据地址 (应该与 s1 的一样)
         println!("s2 指向的堆地址: {:p}", s2.as_ptr());
         // 验证：打印 s2 变量本身的栈地址 (应该与 s1 的不一样)
         println!("s2 变量的栈地址: {:p}", &s2);
-        
+
         take_ownership(&mut s2);
         println!("{s2}, function out!");
     }
